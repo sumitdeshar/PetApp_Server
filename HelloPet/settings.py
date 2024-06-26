791803983d7bf0b27eeb9ca3fc041c86704d479a
Sumit Deshar <sumeetdsr@gmail.com>
2024-06-26 11:09:01 +0545

    adjust allowed host

--- HelloPet/settings.py
+++ HelloPet/settings.py
@@ -10,8 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.environ.get("DEBUG", default=False).lower()
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1']
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["petapp-server.onrender.com",'10.0.2.2', '127.0.0.1']
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 CSRF_COOKIE_SECURE = False
 
 # Application definition
